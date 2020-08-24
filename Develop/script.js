d6172477252987ea310e39645600d903ab3532d8
Skelatorian <mylynhle21@gmail.com>
2020-08-23 21:40:37 -0400

    added action to input and for loop

--- Develop/script.js
+++ Develop/script.js
@@ -25,25 +25,62 @@ function generatePassword() {
   var confirmLength = (prompt("How many characters would you like your password to contain?"));
 
 //loop back if the users inputs outside the parameter with a message here
+//fix the bug where the user can type in "erugihj" and it will go through
 
-while(confirmLength <= 8 || confirmLength >= 128) {
+while(confirmLength <=8 || confirmLength >=128) {
   alert("The password length must be between 8-128 characters. Try again.");
   var confirmLength = (prompt("How many characters would you like your password to contain?"));
   } 
 
 //User alert to confirm with the user how many characters they will have.
-alert(`Your password will have ${confirmLength} characters`);
+  alert(`Your password will have ${confirmLength} characters`);
+
+// Setting parameters of the password based on critera
+var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters.");
+var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters.");    
+var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters.");
+var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters.");
+
+  // The loop if input from user is outside the parameters 
+  while(confirmUpperCase === false && confirmLowerCase === false && confirmSpecialCharacter === false && confirmNumericCharacter === false) {
+    alert("You must choose at least one parameter");
+    var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters.");
+    var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters.");    
+    var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters.");
+    var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters.");   
+} 
+
+// Assign action from user input to password gen
+  var passwordCharacters = []
+  
+if (confirmSpecialCharacter) {
+  passwordCharacters = passwordCharacters.concat(specialChar)
+}
+
+if (confirmNumericCharacter) {
+  passwordCharacters = passwordCharacters.concat(number)
+}
+  
+if (confirmLowerCase) {
+  passwordCharacters = passwordCharacters.concat(alphaLower)
+}
+
+if (confirmUpperCase) {
+  passwordCharacters = passwordCharacters.concat(alphaUpper)
+}
+
+  console.log(passwordCharacters);
 
 
-
-
-
-
-
-
-
-
-
+      // Empty string to be filled based on for loop selecting random characters from the array
+      var randomPassword = ""
+      
+      for (var i = 0; i < confirmLength; i++) {
+        randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random()*passwordCharacters.length)];
+        console.log(randomPassword)
+      }
+      return randomPassword;
+}
 
 
 // Write password to the #password input
@@ -54,7 +91,7 @@ function writePassword() {
   passwordText.value = password;
 
 }
+//AAAAHHHHHHHHHHHHHHHHHHHHHHHHHHHH
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-}
