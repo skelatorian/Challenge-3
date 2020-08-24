cac2b382bc839fd3f4036635a0757dda1086f795
Skelatorian <mylynhle21@gmail.com>
2020-08-23 20:04:04 -0400

    added and determnined arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,30 @@
 // Assignment code here
-
+var generateBtn = document.querySelector("#generate");
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+//Determining the Arrays (4)
+
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!","@","#","$","%","^","&","*","(",")","-","+","/","<",">","?"];
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
